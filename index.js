1a33a40664fc2199055e05b2bb223a39b8612962
Poncho Cuevas <poncho_nov@hotmail.com>
2019-08-22 16:08:57 -0500

    cer

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  push: {
+  ios: {
+    pfx: '', // The filename of private key and certificate in PFX or PKCS12 format from disk  
+    passphrase: '', // optional password to your p12
+    cert: '/push/push.pem', // If not using the .p12 format, the path to the certificate PEM to load from disk
+    key: '', // If not using the .p12 format, the path to the private key PEM to load from disk
+    bundleId: 'com.ioconnect.reminder', // The bundle identifier associated with your app
+    production: false // Specifies which APNS environment to connect to: Production (if true) or Sandbox
+  }
+}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
